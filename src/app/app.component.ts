9adfac8d7b03f4ad6d26d93aa22421138f3a5480
W4TCH0UT <ateekujjawal@gmail.com>
2020-06-20 14:47:16 +0530

    First
    Signed-off-by: W4TCH0UT <ateekujjawal@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import * as XLSX from 'xlsx';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatSort} from '@angular/material/sort';
+
+export interface MonitorData {
+  COL1: number;
+  COL2: number;
+  COL3: string;
+  COL4: string;
+  COL5: string;
+  COL6: string;
+  COL7: string;
+  COL18: string;
+  COL19: number;
+  COL20: number;
+  DESCRIPTION: string;
+  ENABLED_FLAG: string;
+  LOOKUP_TYPE: string;
+  LOOKUP_CODE: string;
+  START_DATE: Date;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +28,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dell-test';
+  name = 'This is XLSX TO JSON CONVERTER';
+  willDownload = false;
+  displayedColumns: string[] = ['LOOKUP_TYPE','LOOKUP_CODE','DESCRIPTION','ENABLED_FLAG', 'START_DATE', 'COL1', 'COL2', 'COL3', 'COL4', 'COL5', 'COL6', 'COL7', 'COL18', 'COL19', 'COL20'];
+  jsonData: MonitorData[] = null;
+  dataSource = null;
+
+  @ViewChild(MatPaginator, {static: false})
+  set paginator(value: MatPaginator) {
+    this.dataSource.paginator = value;
+  }
+
+  @ViewChild(MatSort, {static: false})
+  set sort(value1: MatSort) {
+  	this.dataSource.sort = value1
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  onFileChange(ev) {
+    let workBook = null;
+    let tempData = null;
+    const reader = new FileReader();
+    const file = ev.target.files[0];
+    reader.onload = (event) => {
+      const data = reader.result;
+      workBook = XLSX.read(data, { type: 'binary' , cellDates: true, dateNF: 'yyyy/mm/dd;@'});
+      tempData = workBook.SheetNames.reduce((initial, name) => {
+        const sheet = workBook.Sheets[name];
+        initial[name] = XLSX.utils.sheet_to_json(sheet);
+        console.log(sheet)
+        return initial[name];
+      }, {});
+      tempData = tempData.map(({LOOKUP_TYPE,LOOKUP_CODE,DESCRIPTION,ENABLED_FLAG, START_DATE, COL1, COL2, COL3, COL4,COL5, COL6, COL7, COL18, COL19, COL20}) => ({LOOKUP_TYPE,LOOKUP_CODE,DESCRIPTION,ENABLED_FLAG, START_DATE, COL1, COL2, COL3, COL4,COL5, COL6, COL7, COL18, COL19, COL20}));
+      const dataString = JSON.stringify(tempData);
+      this.jsonData = tempData;
+      this.dataSource = new MatTableDataSource<MonitorData>(this.jsonData);
+      console.log(this.jsonData)
+    }
+    reader.readAsBinaryString(file);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
