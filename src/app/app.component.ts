bd57ed5fdf8e75a038cb549d6714ba51cea8243b
W4TCH0UT <ateekujjawal@gmail.com>
2020-07-04 19:54:30 +0530

    Some UI changes and fixes
    Signed-off-by: W4TCH0UT <ateekujjawal@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,23 +5,26 @@ import {MatTableDataSource} from '@angular/material/table';
 import {MatSort} from '@angular/material/sort';
 import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
 import { Observable, merge } from 'rxjs';
+import * as mdata from './data.json';
+
 
 export interface MonitorData {
-  COL1: number;
-  COL2: number;
+  COL1: string;
+  COL2: string;
   COL3: string;
   COL4: string;
   COL5: string;
   COL6: string;
   COL7: string;
   COL18: string;
-  COL19: number;
-  COL20: number;
+  COL19: string;
+  COL20: string;
   DESCRIPTION: string;
   ENABLED_FLAG: string;
   LOOKUP_TYPE: string;
   LOOKUP_CODE: string;
-  START_DATE: Date;
+  START_DATE: string;
+  STATUS: string;
 }
 
 @Component({
@@ -30,13 +33,11 @@ export interface MonitorData {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'This is XLSX TO JSON CONVERTER';
-  willDownload = false;
-  displayedColumns: string[] = ['LOOKUP_TYPE','LOOKUP_CODE','DESCRIPTION','ENABLED_FLAG', 'START_DATE', 'COL1', 'COL2', 'COL3', 'COL4', 'COL5', 'COL6', 'COL7', 'COL18', 'COL19', 'COL20'];
   jsonData: MonitorData[] = null;
   dataSource = null;
 
   form:FormGroup = new FormGroup({
+    STATUS: new FormControl(false),
     LOOKUP_TYPE: new FormControl(false),
     LOOKUP_CODE: new FormControl(false),
     START_DATE: new FormControl(false),
@@ -54,6 +55,7 @@ export class AppComponent {
     COL20: new FormControl(false)
   });
 
+  STATUS = this.form.get('STATUS');
   LOOKUP_TYPE = this.form.get('LOOKUP_TYPE');
   LOOKUP_CODE = this.form.get('LOOKUP_CODE');
   START_DATE = this.form.get('START_DATE');
@@ -70,24 +72,23 @@ export class AppComponent {
   COL19 = this.form.get('COL19');
   COL20 = this.form.get('COL20');
 
-  cbValues;
-
   columnDefinitions = [
-    { def: 'LOOKUP_TYPE', label: 'LookupType', hide: this.LOOKUP_TYPE.value},
-    { def: 'LOOKUP_CODE', label: 'MonitorID', hide: this.LOOKUP_CODE.value},
-    { def: 'START_DATE', label: 'StartDate', hide: this.START_DATE.value},
-    { def: 'ENABLED_FLAG', label: 'EnabledFlag', hide: this.ENABLED_FLAG.value},
-    { def: 'DESCRIPTION', label: 'MailBody', hide: this.DESCRIPTION.value},
-    { def: 'COL1', label: 'Col1', hide: this.COL1.value},
-    { def: 'COL2', label: 'OrderBy', hide: this.COL2.value},
-    { def: 'COL3', label: 'MonitorType', hide: this.COL3.value},
-    { def: 'COL4', label: 'DateSelectType', hide: this.COL4.value},
-    { def: 'COL5', label: 'FromEmail', hide: this.COL5.value},
-    { def: 'COL6', label: 'ToEmail', hide: this.COL6.value},
-    { def: 'COL7', label: 'Col7', hide: this.COL7.value},
-    { def: 'COL18', label: 'Col18', hide: this.COL18.value},
-    { def: 'COL19', label: 'LastFoundDate', hide: this.COL19.value},
-    { def: 'COL20', label: 'LastRunDate', hide: this.COL20.value},
+    { def: 'STATUS', label: 'Status', hide: this.STATUS.value},
+    { def: 'LOOKUP_TYPE', label: 'Lookup Type', hide: this.ENABLED_FLAG.value},
+    { def: 'LOOKUP_CODE', label: 'Monitor ID', hide: this.ENABLED_FLAG.value},
+    { def: 'START_DATE', label: 'Start Date', hide: this.ENABLED_FLAG.value},
+    { def: 'ENABLED_FLAG', label: 'Enabled Flag', hide: this.ENABLED_FLAG.value},
+    { def: 'DESCRIPTION', label: 'Mail Body', hide: this.DESCRIPTION.value},
+    { def: 'COL1', label: 'Interval(minutes)', hide: this.COL1.value},
+    { def: 'COL2', label: 'Order By', hide: this.COL2.value},
+    { def: 'COL3', label: 'Monitor Type', hide: this.COL3.value},
+    { def: 'COL4', label: 'Date Select Type', hide: this.COL4.value},
+    { def: 'COL5', label: 'From Email', hide: this.COL5.value},
+    { def: 'COL6', label: 'To Email', hide: this.COL6.value},
+    { def: 'COL7', label: 'Col 7', hide: this.COL7.value},
+    { def: 'COL18', label: 'Last Run Status', hide: this.COL18.value},
+    { def: 'COL19', label: 'Last Found Date', hide: this.COL19.value},
+    { def: 'COL20', label: 'Last Run Date', hide: this.COL20.value},
   ]
 
   @ViewChild(MatPaginator, {static: false})
@@ -99,68 +100,63 @@ export class AppComponent {
     return this.columnDefinitions.filter(cd=>!cd.hide).map(cd=>cd.def);
   }
 
-  ngAfterViewInit() {
-   let o1:Observable<boolean> = this.LOOKUP_TYPE.valueChanges;
-   let o2:Observable<boolean> = this.LOOKUP_CODE.valueChanges;
-   let o3:Observable<boolean> = this.START_DATE.valueChanges;
-   let o4:Observable<boolean> = this.ENABLED_FLAG.valueChanges;
-   let o5:Observable<boolean> = this.DESCRIPTION.valueChanges;
-   let o6:Observable<boolean> = this.COL1.valueChanges;
-   let o7:Observable<boolean> = this.COL2.valueChanges;
-   let o8:Observable<boolean> = this.COL3.valueChanges;
-   let o9:Observable<boolean> = this.COL4.valueChanges;
-   let o10:Observable<boolean> = this.COL5.valueChanges;
-   let o11:Observable<boolean> = this.COL6.valueChanges;
-   let o12:Observable<boolean> = this.COL7.valueChanges;
-   let o13:Observable<boolean> = this.COL18.valueChanges;
-   let o14:Observable<boolean> = this.COL19.valueChanges;
-   let o15:Observable<boolean> = this.COL20.valueChanges;
+  constructor() {
+    let tempData = mdata["Sheet1"];
+    console.log(tempData);
+    this.jsonData = tempData.map(
+      ({LOOKUP_TYPE,LOOKUP_CODE,DESCRIPTION,ENABLED_FLAG, START_DATE, COL1, COL2, COL3, COL4,COL5, COL6, COL7, COL18, COL19, COL20}) =>
+      ({LOOKUP_TYPE,LOOKUP_CODE,DESCRIPTION,ENABLED_FLAG, START_DATE, COL1, COL2, COL3, COL4,COL5, COL6, COL7, COL18, COL19, COL20, STATUS : "null"}));
+    this.jsonData.forEach((element) => {
+      if(element.COL19 == element.COL20)
+        element.STATUS = "red";
+      else
+        element.STATUS = "green";
+    })
+    this.dataSource = new MatTableDataSource<MonitorData>(this.jsonData);
+    console.log(this.jsonData)
+    let o1:Observable<boolean> = this.LOOKUP_TYPE.valueChanges;
+     let o2:Observable<boolean> = this.LOOKUP_CODE.valueChanges;
+     let o3:Observable<boolean> = this.START_DATE.valueChanges;
+     let o4:Observable<boolean> = this.ENABLED_FLAG.valueChanges;
+     let o5:Observable<boolean> = this.DESCRIPTION.valueChanges;
+     let o6:Observable<boolean> = this.COL1.valueChanges;
+     let o7:Observable<boolean> = this.COL2.valueChanges;
+     let o8:Observable<boolean> = this.COL3.valueChanges;
+     let o9:Observable<boolean> = this.COL4.valueChanges;
+     let o10:Observable<boolean> = this.COL5.valueChanges;
+     let o11:Observable<boolean> = this.COL6.valueChanges;
+     let o12:Observable<boolean> = this.COL7.valueChanges;
+     let o13:Observable<boolean> = this.COL18.valueChanges;
+     let o14:Observable<boolean> = this.COL19.valueChanges;
+     let o15:Observable<boolean> = this.COL20.valueChanges;
+     let o16:Observable<boolean> = this.STATUS.valueChanges;
 
-   merge(o1, o2, o3, o4, o5, o6, o7, o8, o9, o10, o11, o12, o13, o14, o15).subscribe( v=>{
-   this.columnDefinitions[0].hide = this.LOOKUP_TYPE.value;
-   this.columnDefinitions[1].hide = this.LOOKUP_CODE.value;
-   this.columnDefinitions[2].hide = this.START_DATE.value;  
-   this.columnDefinitions[3].hide = this.ENABLED_FLAG.value;  
-   this.columnDefinitions[4].hide = this.DESCRIPTION.value;  
-   this.columnDefinitions[5].hide = this.COL1.value;  
-   this.columnDefinitions[6].hide = this.COL2.value;  
-   this.columnDefinitions[7].hide = this.COL3.value;  
-   this.columnDefinitions[8].hide = this.COL4.value;  
-   this.columnDefinitions[9].hide = this.COL5.value;  
-   this.columnDefinitions[10].hide = this.COL6.value;  
-   this.columnDefinitions[11].hide = this.COL7.value;  
-   this.columnDefinitions[12].hide = this.COL18.value;  
-   this.columnDefinitions[13].hide = this.COL19.value;  
-   this.columnDefinitions[14].hide = this.COL20.value;  
-      console.log(this.columnDefinitions);
-    });
+     merge(o1, o2, o3, o4, o5, o6, o7, o8, o9, o10, o11, o12, o13, o14, o15, o16).subscribe( v=>{
+     this.columnDefinitions[0].hide = this.STATUS.value;
+     this.columnDefinitions[1].hide = this.LOOKUP_TYPE.value;
+     this.columnDefinitions[2].hide = this.LOOKUP_CODE.value;
+     this.columnDefinitions[3].hide = this.START_DATE.value;  
+     this.columnDefinitions[4].hide = this.ENABLED_FLAG.value;  
+     this.columnDefinitions[5].hide = this.DESCRIPTION.value;  
+     this.columnDefinitions[6].hide = this.COL1.value;  
+     this.columnDefinitions[7].hide = this.COL2.value;  
+     this.columnDefinitions[8].hide = this.COL3.value;  
+     this.columnDefinitions[9].hide = this.COL4.value;  
+     this.columnDefinitions[10].hide = this.COL5.value;  
+     this.columnDefinitions[11].hide = this.COL6.value;  
+     this.columnDefinitions[12].hide = this.COL7.value;  
+     this.columnDefinitions[13].hide = this.COL18.value;  
+     this.columnDefinitions[14].hide = this.COL19.value;  
+     this.columnDefinitions[15].hide = this.COL20.value;  
+        console.log(this.columnDefinitions);
+      });
+    }
+
+  ngAfterViewInit() {
   }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-
-  onFileChange(ev) {
-    let workBook = null;
-    let tempData = null;
-    const reader = new FileReader();
-    const file = ev.target.files[0];
-    reader.onload = (event) => {
-      const data = reader.result;
-      workBook = XLSX.read(data, { type: 'binary' , cellDates: true, dateNF: 'yyyy/mm/dd;@'});
-      tempData = workBook.SheetNames.reduce((initial, name) => {
-        const sheet = workBook.Sheets[name];
-        initial[name] = XLSX.utils.sheet_to_json(sheet);
-        console.log(sheet)
-        return initial[name];
-      }, {});
-      tempData = tempData.map(({LOOKUP_TYPE,LOOKUP_CODE,DESCRIPTION,ENABLED_FLAG, START_DATE, COL1, COL2, COL3, COL4,COL5, COL6, COL7, COL18, COL19, COL20}) => ({LOOKUP_TYPE,LOOKUP_CODE,DESCRIPTION,ENABLED_FLAG, START_DATE, COL1, COL2, COL3, COL4,COL5, COL6, COL7, COL18, COL19, COL20}));
-      const dataString = JSON.stringify(tempData);
-      this.jsonData = tempData;
-      this.dataSource = new MatTableDataSource<MonitorData>(this.jsonData);
-      console.log(this.jsonData)
-    }
-    reader.readAsBinaryString(file);
   }
 }
